5bc6d457cb609a0faf002795a2e3246a4b614375
teeaaspoon <tommypoon36@gmail.com>
2018-06-04 22:02:26 +0000

    refactored function

--- loopylighthouse2.js
+++ loopylighthouse2.js
@@ -1,3 +1,70 @@
+function loopylighthouse_refactor(range, multiples, words) {
+    // range check
+    if (!Array.isArray(range)) {
+        console.log("Please enter the range in an array");
+        return;
+    }
+    if (range.some(isNaN)) {
+        console.log("Please enter a valid range");
+        return;
+    }
+    if (range[0] == undefined || range[1] == undefined) {
+        console.log("Please enter a valid range");
+        return;
+    } else if (range[0] > range[1]) {
+        console.log("Please enter a valid range");
+        return;
+    }
+    // multiples check
+    if (!Array.isArray(multiples)) {
+        console.log("Please enter 2 multiples in an array");
+        return;
+    }
+    if (multiples.some(isNaN)) {
+        console.log("Please enter a valid multiples");
+        return;
+    }
+    if (multiples[0] == undefined || multiples[1] == undefined) {
+        console.log("Please enter a valid multiples");
+        return;
+    }
+    // words check
+    if (!Array.isArray(words)) {
+        console.log("Please enter 2 words in an array");
+        return;
+    }
+    if (words[0] == undefined || words[1] == undefined) {
+        console.log("Please enter 2 valid words");
+        return;
+    }
+
+    console.log(
+        "We will log all numbers from " +
+            range[0] +
+            " to " +
+            range[1] +
+            " if a number is a multiple of " +
+            multiples[0] +
+            " it will be replaced with " +
+            words[0] +
+            " if it is a multiple of " +
+            multiples[1] +
+            " it will be replaced with " +
+            words[1]
+    );
+
+    for (var num = range[0]; num < range[1]; num++) {
+        var output = "";
+        if (num % multiples[0] === 0) {
+            output += words[0];
+        }
+        if (num % multiples[1] === 0) {
+            output += words[1];
+        }
+        console.log(output === "" ? num : output);
+    }
+}
+
 function loopylighthouse(range, multiples, words) {
     // range check
     if (!Array.isArray(range)) {
@@ -38,9 +105,20 @@ function loopylighthouse(range, multiples, words) {
         return;
     }
 
-    console.log("We will log all numbers from " + range[0] + " to " + range[1] + " if a number is a multiple of " + multiples[0] + " it will be replaced with " + words[0] + " if it is a multiple of " + multiples[1] + " it will be replaced with " + words[1]);
-
-
+    console.log(
+        "We will log all numbers from " +
+            range[0] +
+            " to " +
+            range[1] +
+            " if a number is a multiple of " +
+            multiples[0] +
+            " it will be replaced with " +
+            words[0] +
+            " if it is a multiple of " +
+            multiples[1] +
+            " it will be replaced with " +
+            words[1]
+    );
 
     var number = range[0];
     while (number <= range[1]) {
@@ -61,3 +139,16 @@ function loopylighthouse(range, multiples, words) {
 }
 
 loopylighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
+
+//TEST
+console.time("ORIGINAL Function");
+for (var index = 0; index < 10; index++) {
+    loopylighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
+}
+console.timeEnd("ORIGINAL Function");
+
+console.time("REFACTORED Function");
+for (var index = 0; index < 10; index++) {
+    loopylighthouse_refactor([15, 90], [2, 5], ["Batty", "Beacon"]);
+}
+console.timeEnd("REFACTORED Function");
